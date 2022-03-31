00c7feff5c126d0c123d521e36262f3f7de08e9f
JD Bothma <jbothma@gmail.com>
2022-03-31 18:10:56 +0200

    Print all pages of results

--- timeline/app.py
+++ timeline/app.py
@@ -8,6 +8,23 @@ import argparse
 MESSAGE_PREVIEW_LENGTH = 50
 CHANNEL_PREVIEW_LENGTH = 10
 
+
+def get_page(page_num):
+    return app.client.search_messages(query="from:jd after:February", sort="timestamp", page=page_num)
+
+def print_response(response):
+    for match in response["messages"]["matches"]:
+        timestamp = datetime.fromtimestamp(float(match['ts']), timezone.utc)
+        message = match['text']
+        message.replace("\r\n", " ")
+        print((
+            f"{ timestamp.astimezone() } "
+            f"#{ match['channel']['name'][:CHANNEL_PREVIEW_LENGTH].ljust(CHANNEL_PREVIEW_LENGTH) } "
+            f"{ message[:MESSAGE_PREVIEW_LENGTH].ljust(MESSAGE_PREVIEW_LENGTH) } "
+            f"{ match['permalink'] } "
+        ))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default="~/.timeline.json")
@@ -18,14 +35,8 @@ if __name__ == "__main__":
 
     app = App(token=config["slack"]["user_token"])
 
-    response = app.client.search_messages(query="from:jd after:February", sort="timestamp")
-    for match in response["messages"]["matches"]:
-        timestamp = datetime.fromtimestamp(float(match['ts']), timezone.utc)
-        message = match['text']
-        message.replace("\n", " ")
-        print((
-            f"{ timestamp.astimezone() } "
-            f"#{ match['channel']['name'][:CHANNEL_PREVIEW_LENGTH].ljust(CHANNEL_PREVIEW_LENGTH) } "
-            f"{ message[:MESSAGE_PREVIEW_LENGTH].ljust(MESSAGE_PREVIEW_LENGTH) } "
-            f"{ match['permalink'] } "
-        ))
+    response = get_page(1)
+    print_response(response)
+
+    for page_num in range(2, response["messages"]["pagination"]["page_count"] + 1):
+        print_response(get_page(page_num))
